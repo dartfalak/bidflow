b0b90283a8e7f767237a9ba1f406608f8a8f607f
dartfalak <falakcles@gmail.com>
2026-05-08 20:38:06 +0500

    add hero section

--- main.js
+++ main.js
@@ -2,3 +2,8 @@ const modal = document.getElementById("signin-modal");
 const closeBtn = document.querySelector(".close-btn");
 
 const authButtons = document.querySelectorAll(".trigger-auth");
+
+authButtons.forEach(button => {
+    button.addEventListener("click", (e) => {
+        e.preventDefault();
+        
