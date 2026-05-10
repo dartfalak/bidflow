1ddcf3ebfa1d003a686a130673cc9cf8099c5d71
dartfalak <falakcles@gmail.com>
2026-05-10 21:59:59 +0500

    add sign-up page

--- main.js
+++ main.js
@@ -1,9 +1,9 @@
-// Firebase imports
+
 import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
 import { getAuth, signInWithEmailAndPassword } from "firebase/auth";
 
-// Firebase config
+
 const firebaseConfig = {
   apiKey: "AIzaSyAPZVbfzvBKtyM3JoGyLgsgI7mU53QFpa0",
   authDomain: "bidflow-82f6c.firebaseapp.com",
@@ -14,23 +14,22 @@ const firebaseConfig = {
   measurementId: "G-RJSM4LH6DP"
 };
 
-// Initialize Firebase
+
 const app = initializeApp(firebaseConfig);
 const analytics = getAnalytics(app);
 
-// Auth
+
 const auth = getAuth(app);
 
-// Elements
 let email = document.querySelector("#email");
 let pass = document.querySelector("#pass");
 let msg = document.querySelector("#msg");
 let enter = document.querySelector("#signin-btn");
 
-// Sign in
+
 enter.addEventListener("click", function (e) {
 
-  // STOP PAGE RELOAD
+  
   e.preventDefault();
 
   signInWithEmailAndPassword(auth, email.value, pass.value)
