f53afdb6ee331567f07c155c082fe34b2af8afb1
dartfalak <falakcles@gmail.com>
2026-05-11 20:35:26 +0500

    add sign-up page

--- main.js
+++ main.js
@@ -1,26 +1,4 @@
 
-// import { initializeApp } from "firebase/app";
-// import { getAnalytics } from "firebase/analytics";
-// import { getAuth, signInWithEmailAndPassword } from "firebase/auth";
-
-
-// const firebaseConfig = {
-//   apiKey: "AIzaSyAPZVbfzvBKtyM3JoGyLgsgI7mU53QFpa0",
-//   authDomain: "bidflow-82f6c.firebaseapp.com",
-//   projectId: "bidflow-82f6c",
-//   storageBucket: "bidflow-82f6c.firebasestorage.app",
-//   messagingSenderId: "530618538215",
-//   appId: "1:530618538215:web:a0e1d9a7e59e08438b6a42",
-//   measurementId: "G-RJSM4LH6DP"
-// };
-
-
-// const app = initializeApp(firebaseConfig);
-// const analytics = getAnalytics(app);
-
-
-// const auth = getAuth(app);
-
 let email = document.querySelector("#email");
 let pass = document.querySelector("#pass");
 let msg = document.querySelector("#msg");
