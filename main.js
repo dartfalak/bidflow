4d5eb275c130f8f2ef54ca00475e221c4af74dba
dartfalak <falakcles@gmail.com>
2026-05-11 20:25:32 +0500

    add sign-up page

--- main.js
+++ main.js
@@ -1,22 +1,22 @@
 
-import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
-import { getAuth, signInWithEmailAndPassword } from "firebase/auth";
+// import { initializeApp } from "firebase/app";
+// import { getAnalytics } from "firebase/analytics";
+// import { getAuth, signInWithEmailAndPassword } from "firebase/auth";
 
 
-const firebaseConfig = {
-  apiKey: "AIzaSyAPZVbfzvBKtyM3JoGyLgsgI7mU53QFpa0",
-  authDomain: "bidflow-82f6c.firebaseapp.com",
-  projectId: "bidflow-82f6c",
-  storageBucket: "bidflow-82f6c.firebasestorage.app",
-  messagingSenderId: "530618538215",
-  appId: "1:530618538215:web:a0e1d9a7e59e08438b6a42",
-  measurementId: "G-RJSM4LH6DP"
-};
+// const firebaseConfig = {
+//   apiKey: "AIzaSyAPZVbfzvBKtyM3JoGyLgsgI7mU53QFpa0",
+//   authDomain: "bidflow-82f6c.firebaseapp.com",
+//   projectId: "bidflow-82f6c",
+//   storageBucket: "bidflow-82f6c.firebasestorage.app",
+//   messagingSenderId: "530618538215",
+//   appId: "1:530618538215:web:a0e1d9a7e59e08438b6a42",
+//   measurementId: "G-RJSM4LH6DP"
+// };
 
 
-const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
+// const app = initializeApp(firebaseConfig);
+// const analytics = getAnalytics(app);
 
 
 const auth = getAuth(app);
